b4f1a3725c1c1cca2008fd3f8e7cdd8232cf392b
express-mongodb <tuyentdph23684@fpt.edu.vn>
2024-07-20 21:20:50 +0700

    tuyen
    loginWallet

--- Server/controller/walletCtrl.js
+++ Server/controller/walletCtrl.js
@@ -1,6 +1,6 @@
 const walletModel = require('../models/walletModels');
-
-async function getWalletInfo(req, res) {
+const axios = require("axios");
+exports.getWalletInfo = async (req, res, next) => {
     try {
         const { address } = req.params;
         const walletInfo = await walletModel.getWalletInfo(address);
@@ -9,7 +9,50 @@ async function getWalletInfo(req, res) {
         res.status(500).json({ error: error.message });
     }
 }
+exports.connectWallet = async (req, res, next) => {
+    const PRIV_KEY = "0q9Ah3X8KCpZfxah-r";
+    const wallet = req.params.id;
 
-module.exports = {
-    getWalletInfo,
+    if (!wallet) {
+        return res.render('Profile/profile', {
+            success: false,
+            message: "Please connect your wallet.",
+            solBalance: null,
+            numTokens: 0,
+            tokens: [],
+            numNFTs: 0,
+            nfts: []
+        });
+    }
+    
+
+    const myHeaders = {
+        headers: {
+            "x-api-key": PRIV_KEY
+        }
+    };
+    try {
+        const response = await axios.get(
+            `https://api.shyft.to/sol/v1/wallet/get_portfolio?network=devnet&wallet=${wallet}`,
+            myHeaders
+        );
+        const result = response.data;
+        if (!result.result || !result.result.sol_balance || !result.result.tokens || !result.result.nfts) {
+            throw new Error("Unexpected response structure from API");
+        }
+
+        res.render('Profile/profile', {
+            success: result.success,
+            message: result.message,
+            solBalance: result.result.sol_balance,
+            numTokens: result.result.num_tokens,
+            tokens: result.result.tokens,
+            numNFTs: result.result.num_nfts,
+            nfts: result.result.nfts,
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("An error occurred");
+    }
 };
+
